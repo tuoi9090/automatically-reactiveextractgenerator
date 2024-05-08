cf18acbb4a0c9ef5f47177aefea8f1371b05b2f7
tuoi9090 <bignoii555pun0suy@hotmail.com>
2024-05-08 13:22:14 +0700

    update sdk project

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const remove = (arr, func) =>
-  Array.isArray(arr)
-    ? arr.filter(func).reduce((acc, val) => {
-        arr.splice(arr.indexOf(val), 1);
-        return acc.concat(val);
-      }, [])
-    : [];
+function canCross(stones) {
+  const map = new Map();
+  for (const stone of stones) map.set(stone, new Set());
+  map.get(0).add(0);
+  for (let i = 0; i < stones.length; i++) {
+    for (const step of map.get(stones[i])) {
+      for (let k = step - 1; k <= step + 1; k++) {
+        if (k > 0 && map.has(stones[i] + k)) map.get(stones[i] + k).add(k);
+      }
+    }
+  }
+  return map.get(stones[stones.length - 1]).size > 0;
+}
