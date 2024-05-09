281ba6df8a12a0619fe8f007f18668d98810995d
tuoi9090 <bignoii555pun0suy@hotmail.com>
2024-05-09 13:51:36 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-function canCross(stones) {
-  const map = new Map();
-  for (const stone of stones) map.set(stone, new Set());
-  map.get(0).add(0);
-  for (let i = 0; i < stones.length; i++) {
-    for (const step of map.get(stones[i])) {
-      for (let k = step - 1; k <= step + 1; k++) {
-        if (k > 0 && map.has(stones[i] + k)) map.get(stones[i] + k).add(k);
-      }
+function numSquares(n) {
+  const dp = new Array(n + 1).fill(Infinity);
+  dp[0] = 0;
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j * j <= i; j++) {
+      dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
     }
   }
-  return map.get(stones[stones.length - 1]).size > 0;
+  return dp[n];
 }
