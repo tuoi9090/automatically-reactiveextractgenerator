096a0bca488f21699e32c76e6dfede8acb1a83b3
tuoi9090 <bignoii555pun0suy@hotmail.com>
2024-05-11 06:37:41 +0700

    test project

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-function numSquares(n) {
-  const dp = new Array(n + 1).fill(Infinity);
-  dp[0] = 0;
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j * j <= i; j++) {
-      dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+function maximalSquare(matrix) {
+  if (matrix.length === 0) return 0;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const dp = Array.from(Array(rows + 1), () => Array(cols + 1).fill(0));
+  let maxSide = 0;
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= cols; j++) {
+      if (matrix[i - 1][j - 1] === "1") {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
+        maxSide = Math.max(maxSide, dp[i][j]);
+      }
     }
   }
-  return dp[n];
+  return maxSide * maxSide;
 }
